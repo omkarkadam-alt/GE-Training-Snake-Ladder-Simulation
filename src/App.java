3529ad977418dd466d63f189a6ca408ec63babff
Omkar Kadam <kadam.omkar@outlook.com>
2023-12-21 00:33:29 +0530

    Initialized the start position

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Snake and Ladder Game");
+        System.out.println("Starting from Position: 0");
+
+        int START_POSITION = 0;
+
+        int currPosition = START_POSITION;
     }
 }
